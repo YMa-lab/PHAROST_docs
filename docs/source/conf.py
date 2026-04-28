4fe1cec4089e0fc4249ac02539100757db35fb59
Ruohan Wang <88182421+RoKsaNne@users.noreply.github.com>
2026-04-27 20:55:55 -0400

    Update Style

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,7 +30,10 @@ exclude_patterns = []
 
 html_theme = "sphinx_rtd_theme"
 html_static_path = ['_static']
-
+html_css_files = [
+    'custom.css',
+]
 html_theme_options = {
     "style_nav_header_background": "#A3E4E1",  # 你可以换成任意颜色
 }
+
